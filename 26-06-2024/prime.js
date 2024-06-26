d19ac89edf6f996706be60aaf7321251bbd53007
ViniciusFerna <vinicius020206@gmail.com>
2024-06-26 10:05:45 -0300

    Aula01

---
+++ 26-06-2024/prime.js
@@ -0,0 +1,9 @@
+console.log("Teste");
+
+console.log("Vinicius");
+//Comentário JavaScript
+console.log("Esta é a nossa primeira impressão na tela (Não é não)")
+
+//LET => variável que pode ser mudada no futuro
+//Const => variável que sempre será a mesma
+
