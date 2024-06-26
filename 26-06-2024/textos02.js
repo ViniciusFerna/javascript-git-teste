a9f8b6620f99693f3697bc50904024c8e54756ff
ViniciusFerna <vinicius020206@gmail.com>
2024-06-26 11:41:31 -0300

    Atualizacao

---
+++ 26-06-2024/textos02.js
@@ -0,0 +1,15 @@
+let txt = "Estão chegando as provas";
+
+console.log(txt.length);
+
+console.log(txt.indexOf("as")); //Retorna a primeira posição de uma string que tiver no especificado
+
+console.log(txt.lastIndexOf("as")); //Retorna a ultima posição da string especificada, igual o indexOf só que a ultima
+
+console.log(txt.slice(0,5)); //Pega o pedaço informado no () e o corta do resto da string
+
+console.log(txt.replace("provas","férias")); //Troca o primeiro valor informado pelo segundo informado
+
+console.log(txt.toUpperCase()); //Deixa tudo maiúsculo
+
+console.log(txt.toLowerCase()); //Deixa tudo minúsculo
